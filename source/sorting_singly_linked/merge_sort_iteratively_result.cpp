cb17555408f494806a5272af07b5bc1c3fdaca1d
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-02-02 02:35:10 +0100

    Update merge_sort_iteratively_result.cpp

--- source/sorting_singly_linked/merge_sort_iteratively_result.cpp
+++ source/sorting_singly_linked/merge_sort_iteratively_result.cpp
@@ -1,54 +1,60 @@
 ﻿#include "../my_chrono.hpp"
+#include "../my_random.hpp"
 using my_chrono::Timer_clock, my_chrono::Time_point, my_chrono::as_seconds;
-
-#include "../data/Abbreviated_list_displayer.hpp"
-using data::Abbreviated_list_displayer;
 
 #include "shuffled_english_words_list.hpp"
 #include "merge_sort_iteratively.hpp"
 namespace x = oneway_sorting_examples;
-using x::Node, x::List, x::shuffled_english_words_list, x::merge_sort_iteratively;
+using
+    x::english_words_list, x::shuffled_english_words_list,
+    x::Node, x::List, x::merge_sort_iteratively;
+using Words_list_func = auto()->List;
 
-#include <iostream>
-using std::fixed, std::cout, std::clog, std::endl;
+#include <iomanip>          // std::setw
+#include <iostream>         // std::(fixed, cout, clog, endl)    
+#include <limits>           // std::numeric_limits
+#include <optional>         // std::optional
+using
+    std::setw, std::numeric_limits,
+    std::fixed, std::cout, std::clog, std::endl,
+    std::optional;
+
+auto seconds_for( Words_list_func& words_list )
+    -> optional<double>
+{
+    List                words       = words_list();
+    const Time_point    start_time  = Timer_clock::now();
+    merge_sort_iteratively( words );
+    const Time_point    end_time    = Timer_clock::now();
+
+    if( not words.is_sorted() ) {
+        return {};
+    }
+    return as_seconds( end_time - start_time );
+}
 
 auto main()
     -> int
 {
-    List words = shuffled_english_words_list();
+    cout << fixed;
 
-    if(0)
-    {
-        Node* p = words.head;
-        for( int i = 1; i <= 12; ++i ) { p = p->next; }
-        delete_list_and_zero( p->next );
-    }
+    cout    << "Iterative \"natural runs\" merge-sort results in seconds, for "
+            << english_words_list().count() << " words:"
+            << endl;
+    cout << endl;
+    const auto w = setw( 16 );
+    cout << w << "Sorted data:" << w << "Shuffled data:" << w << "Diff:" << endl;
+    for( int i = 1; i <= 12; ++i ) {
+        constexpr double nan = numeric_limits<double>::quiet_NaN();
+        const auto& sorted_words    = *english_words_list;
+        const auto& shuffled_words  = *[]{ return shuffled_english_words_list(); };
 
-    const int n = int( words.count() );
-    clog << n << endl;
-    
-    const Time_point start_time = Timer_clock::now();
-    const int n_passes = merge_sort_iteratively( words );
-    const Time_point end_time = Timer_clock::now();
-    const double n_seconds = as_seconds( end_time - start_time );
-
-    clog << fixed << n_seconds << " seconds, using " << n_passes << " passes." << endl;
-    cout << "Iteratively merge-sorted " << n << " words:" << endl;
-    Abbreviated_list_displayer displayer( cout, n );
-    for( Node* p = words.head; p != nullptr; p = p->next ) {
-        displayer.display( p->value );
-    }
-    cout << "." << endl;
-
-    if( 0 ) {
-        std::string_view previous;
-        for( Node* p = words.head; p != nullptr; p = p->next ) {
-            if( p->value < previous ) {
-                cout << "|";
-            }
-            cout << p->value << " ";
-            previous = p->value;
-        }
-        cout << endl;
+        const double sorted_time    = seconds_for( sorted_words ).value_or( nan );
+        const double shuffled_time  = seconds_for( shuffled_words ).value_or( nan );
+        cout
+            << w << sorted_time
+            << w << shuffled_time
+            << w << shuffled_time - sorted_time
+            << endl;
     }
 }
